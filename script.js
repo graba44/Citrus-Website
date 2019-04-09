5767fed03b2cf0a189f92e5560918fea57e455da
unknown <grabowsky44@gmail.com>
2019-04-09 21:20:07 +0200

    Everything is changed, desktop version is ready

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(function(){
+
     $(".menu").on("click", function(){
         $(this).toggleClass("active");
+        $(".overlay").toggleClass("menu-open");
     });
+
+    $(".nav a").on("click", function(){
+        $(".menu").removeClass("active");
+        $(".overlay").removeClass("menu-open");
+    });
+
 });
